03ad25c1ce63f5787eb9f78f6bae28255e5f7cfd
MUGUNDARAM <mugundaram22td0702@svcet.ac.in>
2026-03-26 10:17:38 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,7 @@
+name = input("Enter your name:")
+password = input("Enter your password:")
+
+if name == "admin"and  password == "1416":
+    print("login successfull !")
+else:
+    print("invalid user.")    
