9de25932c0896d544b89960af09b9d63d2b4ade1
MUGUNDARAM <mugundaram22td0702@svcet.ac.in>
2026-03-26 10:19:18 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,9 @@
+age=int(input("enter age:"))
+if (age >=0 and age <=12):
+    print("child")
+elif(age>=13 and age <=17):
+    print("tennager") 
+elif(age>=18 and age <=59):
+    print("adult")
+else:
+    print("senior citizen")  
